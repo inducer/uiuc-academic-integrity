a754054556acb1e9f0071268caaa86bdab0916df
Andreas Kloeckner <inform@tiker.net>
2024-09-26 09:09:45 -0500

    Switch to ruff linting

--- email-templates/date_utils.py
+++ email-templates/date_utils.py
@@ -8,7 +8,7 @@ def date_by_adding_business_days(
     while business_days_to_add > 0:
         current_date += datetime.timedelta(days=1)
         weekday = current_date.weekday()
-        if weekday >= 5: # sunday = 6
+        if weekday >= 5:  # sunday = 6
             continue
         business_days_to_add -= 1
     return current_date
