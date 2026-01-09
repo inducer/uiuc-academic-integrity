2a7bf54aa3aaecd1bda4b8efc29ba3b58d88ce3c
Andreas Kloeckner <inform@tiker.net>
2026-01-09 15:28:58 -0600

    Centralize grist credentials in a single script

--- av-request.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-exec ./av-request \
-	--grist-root-url https://scicomp-grist.cs.illinois.edu \
-	--grist-doc-id s7VzXiAHXbwgivucYprb6z \
-	--grist-api-key ~/.grist-uiuc-api-key \
-	"$@"
