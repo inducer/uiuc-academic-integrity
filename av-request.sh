f2ba3a749315fa35562a14794a235dab14a35741
Andreas Kloeckner <inform@tiker.net>
2026-01-09 15:15:14 -0600

    Rename av-request script

--- av-request.sh
+++ av-request.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-exec ./create-av-request \
+exec ./av-request \
 	--grist-root-url https://scicomp-grist.cs.illinois.edu \
 	--grist-doc-id s7VzXiAHXbwgivucYprb6z \
 	--grist-api-key ~/.grist-uiuc-api-key \
