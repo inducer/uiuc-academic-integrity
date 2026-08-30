c1d9b6e31b86908b97d0813a7658fb4f1d513b62
Andreas Kloeckner <inform@tiker.net>
2026-08-30 13:39:16 -0500

    Remove some now-unused suppressions

--- email-templates/date_utils.py
+++ email-templates/date_utils.py
@@ -31,11 +31,11 @@ def advance_deadline(
         deadline = None
     else:
         min_bump = existing_days
-        deadline = datetime.datetime.fromtimestamp(old_deadline)  # ruff:ignore[call-datetime-fromtimestamp]
+        deadline = datetime.datetime.fromtimestamp(old_deadline)
 
     min_deadline = datetime_by_adding_business_days(
         datetime.datetime.combine(
-            datetime.date.today(),  # ruff:ignore[call-date-today]
+            datetime.date.today(),
             datetime.time(17, 0)),
         min_bump)
 
